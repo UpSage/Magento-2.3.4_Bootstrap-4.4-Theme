4126db2d731f834c87beed258e7b68bc8e175c79
Topski2015 <aton.christopher05@gmail.com>
2020-04-08 02:13:38 +0800

    Removed Tooltip for Swatches

--- design/frontend/SageKit/theme-bootstrap4/Magento_Swatches/web/js/swatch-renderer.js
+++ design/frontend/SageKit/theme-bootstrap4/Magento_Swatches/web/js/swatch-renderer.js
@@ -78,133 +78,133 @@ define([
  *  - thumb-height
  */
 
- $.widget('mage.SwatchRendererTooltip', {
+//  $.widget('mage.SwatchRendererTooltip', {
 
-  options: {
-   delay: 200, //how much ms before tooltip to show
-   tooltipClass: 'swatch-option-tooltip' //configurable, but remember about css
-  },
+//   options: {
+//    delay: 200, //how much ms before tooltip to show
+//    tooltipClass: 'swatch-option-tooltip' //configurable, but remember about css
+//   },
   
-  /**
-  * @private
-  */
+//   /**
+//   * @private
+//   */
  
-  _init: function () {
+//   _init: function () {
       
-   var $widget = this,
-       $this = this.element,
-       $element = $('.' + $widget.options.tooltipClass),
-       timer,
-       type = parseInt($this.attr('option-type'), 10),
-       label = $this.attr('option-label'),
-       thumb = $this.attr('option-tooltip-thumb'),
-       value = $this.attr('option-tooltip-value'),
-       width = $this.attr('thumb-width'),
-       height = $this.attr('thumb-height'),
-       $image,
-       $title,
-       $corner;
+//    var $widget = this,
+//        $this = this.element,
+//        $element = $('.' + $widget.options.tooltipClass),
+//        timer,
+//        type = parseInt($this.attr('option-type'), 10),
+//        label = $this.attr('option-label'),
+//        thumb = $this.attr('option-tooltip-thumb'),
+//        value = $this.attr('option-tooltip-value'),
+//        width = $this.attr('thumb-width'),
+//        height = $this.attr('thumb-height'),
+//        $image,
+//        $title,
+//        $corner;
        
-   if (!$element.length) {
-    $element = $('<div class="' + $widget.options.tooltipClass + '"><div class="image"></div><div class="title"></div><div class="corner"></div></div>');
-    $('body').append($element);
-   }
+//    if (!$element.length) {
+//     $element = $('<div class="' + $widget.options.tooltipClass + '"><div class="image"></div><div class="title"></div><div class="corner"></div></div>');
+//     $('body').append($element);
+//    }
     
-   $image = $element.find('.image');
-   $title = $element.find('.title');
-   $corner = $element.find('.corner');
+//    $image = $element.find('.image');
+//    $title = $element.find('.title');
+//    $corner = $element.find('.corner');
     
-   $this.hover(function () {
+//    $this.hover(function () {
         
-    if (!$this.hasClass('disabled')) {
-     timer = setTimeout(
-      function () {
+//     if (!$this.hasClass('disabled')) {
+//      timer = setTimeout(
+//       function () {
            
-       var leftOpt = null,
-           leftCorner = 0,
-           left,
-           $window;
+//        var leftOpt = null,
+//            leftCorner = 0,
+//            left,
+//            $window;
             
-       if (type === 2) {
+//        if (type === 2) {
             
-        // Image
-        $image.css({
-         'background': 'url("' + thumb + '") no-repeat center', //Background case
-         'background-size': 'initial',
-         'width': width + 'px',
-         'height': height + 'px'
-        });
+//         // Image
+//         $image.css({
+//          'background': 'url("' + thumb + '") no-repeat center', //Background case
+//          'background-size': 'initial',
+//          'width': width + 'px',
+//          'height': height + 'px'
+//         });
 
-        $image.show();
+//         $image.show();
         
-       } else if (type === 1) {
+//        } else if (type === 1) {
             
-        // Color
-        $image.css({
-         background: value
-        });
+//         // Color
+//         $image.css({
+//          background: value
+//         });
          
-         $image.show(); 
-       } else if (type === 0 || type === 3) {
+//          $image.show(); 
+//        } else if (type === 0 || type === 3) {
             
-        // Default
-        $image.hide(); 
-       }
+//         // Default
+//         $image.hide(); 
+//        }
         
-       $title.text(label);
-       leftOpt = $this.offset().left;
-       left = leftOpt + $this.width() / 2 - $element.width() / 2; 
-       $window = $(window);
+//        $title.text(label);
+//        leftOpt = $this.offset().left;
+//        left = leftOpt + $this.width() / 2 - $element.width() / 2; 
+//        $window = $(window);
         
-       // the numbers (5 and 5) is magick constants for offset from left or right page    
-       if (left < 0) {
-        left = 5; 
-       } else if (left + $element.width() > $window.width()) {    
-        left = $window.width() - $element.width() - 5; 
-       }
+//        // the numbers (5 and 5) is magick constants for offset from left or right page    
+//        if (left < 0) {
+//         left = 5; 
+//        } else if (left + $element.width() > $window.width()) {    
+//         left = $window.width() - $element.width() - 5; 
+//        }
         
-       // the numbers (6,  3 and 18) is magick constants for offset tooltip
-       leftCorner = 0;
+//        // the numbers (6,  3 and 18) is magick constants for offset tooltip
+//        leftCorner = 0;
         
-       if ($element.width() < $this.width()) {
-        leftCorner = $element.width() / 2 - 3;
-       } else {
-        leftCorner = (leftOpt > left ? leftOpt - left : left - leftOpt) + $this.width() / 2 - 6;
+//        if ($element.width() < $this.width()) {
+//         leftCorner = $element.width() / 2 - 3;
+//        } else {
+//         leftCorner = (leftOpt > left ? leftOpt - left : left - leftOpt) + $this.width() / 2 - 6;
         
-       }
+//        }
         
-       $corner.css({
-        left: leftCorner
-       });
+//        $corner.css({
+//         left: leftCorner
+//        });
         
-       $element.css({
-        left: left,
-        top: $this.offset().top - $element.height() - $corner.height() - 18
-       }).show();
+//        $element.css({
+//         left: left,
+//         top: $this.offset().top - $element.height() - $corner.height() - 18
+//        }).show();
     
-      },
+//       },
 
-      $widget.options.delay
-     );
-    }
+//       $widget.options.delay
+//      );
+//     }
     
-   }, function () {
-    $element.hide();
-    clearTimeout(timer);
-   });
+//    }, function () {
+//     $element.hide();
+//     clearTimeout(timer);
+//    });
     
-   $(document).on('tap', function () {
-    $element.hide();
-    clearTimeout(timer);
-   });
+//    $(document).on('tap', function () {
+//     $element.hide();
+//     clearTimeout(timer);
+//    });
     
-   $this.on('tap', function (event) {
-    event.stopPropagation();
-   });
+//    $this.on('tap', function (event) {
+//     event.stopPropagation();
+//    });
 
-  }
+//   }
 
- });
+//  });
  
  /**
  * Render swatch controls with options and use tooltips.
@@ -417,8 +417,8 @@ define([
             var $widget = this,
                 container = this.element,
                 classes = this.options.classes,
-                chooseText = this.options.jsonConfig.chooseText,
-                showTooltip = this.options.showTooltip;
+                chooseText = this.options.jsonConfig.chooseText;
+                // showTooltip = this.options.showTooltip;
 
             $widget.optionsMap = {};
 
@@ -485,12 +485,12 @@ define([
                 });
             });
 
-            if (showTooltip === 1) {
-                // Connect Tooltip
-                container
-                    .find('[option-type="1"], [option-type="2"], [option-type="0"], [option-type="3"]')
-                    .SwatchRendererTooltip();
-            }
+            // if (showTooltip === 1) {
+            //     // Connect Tooltip
+            //     container
+            //         .find('[option-type="1"], [option-type="2"], [option-type="0"], [option-type="3"]')
+            //         .SwatchRendererTooltip();
+            // }
 
             // Hide all elements below more button
             $('.' + classes.moreButton).nextAll().hide();
