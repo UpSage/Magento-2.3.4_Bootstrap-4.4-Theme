115d1b4104c03e22d89356b057764f112b196495
Topski2015 <aton.christopher05@gmail.com>
2020-04-16 22:21:18 +0800

    Updated registration form structure. Added Date field and Select field override

---
+++ code/SageKit/Bootstrap4Date/View/Element/Html/Date.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace SageKit\Bootstrap4Date\View\Element\Html;
+
+class Date extends \Magento\Framework\View\Element\Html\Date {
+    
+ protected function _toHtml() {
+     
+  $html = '<input type="date" name="' . $this->getName() . '" id="' . $this->getId() . '" ';
+  $html .= 'value="' . $this->escapeHtml($this->getValue()) . '" ';
+  $html .= 'class="form-control' . $this->getClass() . '" ' . $this->getExtraParams() . '/> ';
+  $calendarYearsRange = $this->getYearsRange();
+  $changeMonth = $this->getChangeMonth();
+  $changeYear = $this->getChangeYear();
+  $maxDate = $this->getMaxDate();
+  $showOn = $this->getShowOn();
+  $firstDay = $this->getFirstDay();
+  
+  return $html;
+
+ }
+
+}
---
+++ code/SageKit/Bootstrap4Select/View/Element/Html/Select.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace SageKit\Bootstrap4Select\View\Element\Html;
+
+class Select extends \Magento\Framework\View\Element\Html\Select {
+    
+ protected function _toHtml() {
+     
+  if (!$this->_beforeToHtml()) {
+   return '';
+  }
+  
+  $html = '<select name="' . $this->getName() . '" id="' . $this->getId() . '" class="form-control' . $this->getClass() . '" title="' . $this->escapeHtml($this->getTitle()) . '" ' . $this->getExtraParams() . '>';
+  
+  $values = $this->getValue();
+  
+  if (!is_array($values)) {
+   $values = (array)$values;
+  }
+  
+  $isArrayOption = true;
+  
+  foreach ($this->getOptions() as $key => $option) {
+   $optgroupName = '';
+   if ($isArrayOption && is_array($option)) {
+    $value = $option['value'];
+    $label = (string)$option['label'];
+    $optgroupName = isset($option['optgroup-name']) ? $option['optgroup-name'] : $label;
+    $params = !empty($option['params']) ? $option['params'] : [];
+   } else {
+    $value = (string)$key;
+    $label = (string)$option;
+    $isArrayOption = false;
+    $params = [];
+   }
+   if (is_array($value)) {
+    $html .= '<optgroup label="' . $this->escapeHtml($label) . '" data-optgroup-name="' . $this->escapeHtml($optgroupName) . '">';
+    foreach ($value as $keyGroup => $optionGroup) {
+     if (!is_array($optionGroup)) {
+      $optionGroup = ['value' => $keyGroup, 'label' => $optionGroup];
+     }
+     $html .= $this->_optionToHtml($optionGroup, in_array($optionGroup['value'], $values));
+    }
+    $html .= '</optgroup>';
+   } else {
+    $html .= $this->_optionToHtml(
+     ['value' => $value, 'label' => $label, 'params' => $params],
+     in_array($value, $values)
+    );
+   }
+  }
+
+  $html .= '</select>';
+  
+  return $html;
+
+ }
+
+}
